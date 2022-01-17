9f15cd0e476eedf1b2d4c35530317aaa06210359
Samuel Verneck <the.verneck@gmail.com>
2022-01-17 11:43:52 -0300

    fix: add alterations code file server.js

--- #0001/server.js
+++ #0001/server.js
@@ -1,5 +1,5 @@
-/**
- *  1) criar o projeto - npm init - y[x] 
+/** 
+    1) criar o projeto - npm init - y[x] 
     2) instalar o webserver - npm install express nodemon[x] 
     3) criar o servico[x] 
     4) cria a variavel do express[X] 
@@ -13,17 +13,24 @@
         * DELETE[X]
 */
 
-//-- Variaveis globais
+// Variaveis globais
 const express = require('express');
 const server = express();
 const router = express.Router();
 
-//-- Aplicacao de middleware
-server.use(express.json()); //-- Serve para o express saber qual o formato de dados virá no body da requisição
+// Aplicacao de middleware
+server.use(express.json()); // Serve para o express saber qual o formato de dados virá no body da requisição
 
-//-- Criação das Rotas
+// Criação das Rotas
 router.get('/task', (_req, res) => {
-    return res.send('Get Ok!');
+    return res.json({
+        error: false,
+        message: 'Requisição OK!',
+        dataSimple: {
+            name: 'Salmoel'
+        }
+    });
+    // return res.send('Get Ok!');
 });
 
 router.post('/task', (_req, res) => {
@@ -31,17 +38,29 @@ router.post('/task', (_req, res) => {
 });
 
 router.put('/task/:id', (_req, res) => {
-    // Faltou pegar as informacoes enviadas na req (id)
-    return res.send('Put Ok!')
+    const {
+        id
+    } = req.params;
+    const {
+        name
+    } = req.body;
+
+    console.log(id, name);
+    return res.json({
+        message: `Bem vindo,  ${name}! Seu código é ${id}.`
+    });
+    // return res.send('Put Ok!')
 });
 
 router.delete('/task', (_req, res) => {
     // Faltou colocar parametro para saber qual registro será excluido
     // Faltou pegar as informacoes enviadas na req (id)
-    return res.send('Deletet Ok!')
+    const taskId = req.params.id;
+    res.status(200).send(`Requisição recebida com sucesso! ${taskId}`);
+    return res.send('Delete Ok!')
 });
 
 server.use(router);
 
-//-- Configuração do serviço
+// Configuração do serviço
 server.listen(3334, () => console.log('Server running at http://localhost:3334/!'));
